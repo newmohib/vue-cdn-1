c16fcaff14ee19a7e68dabbf4611a942f41053bf
newmohib <newmohib@gmail.com>
2019-10-22 23:54:50 +0600

    update function

--- app3.js
+++ app3.js
@@ -1,18 +1,18 @@
-var myapp=new Vue({
+new Vue({
     el:"#my-app",
     data:{
         name: "Online web Tutoeial",
         playlist:"Vue Js"
     },
     methods:{
-        run: (message)=>{
+        run: function (message){
             return " I am running " + message;
         },
-        welcome: (dayTime)=>{
-            return "Good" + dayTime;
+        welcome: function (dayTime){
+            return   "Good" + dayTime;
         },
-        valueAccess: ()=>{
-            return this.name;
+        accessValue: function (){
+            return   this.name + " "+ this.playlist;
         }
     }
 })
