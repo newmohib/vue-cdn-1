e8bc6a849e45d8e6dcab2ce39a3ff001b30b49e2
newmohib <newmohib@gmail.com>
2019-10-24 02:46:21 +0600

    added v-html ,img, inupt form

---
+++ app4.js
@@ -0,0 +1,15 @@
+var myapp= new Vue({
+    el:"#my-app4",
+    data:{
+        name:"Online web tutor",
+        title:"this is chanel link",
+        imgLink:"assates/img/1.jpg",
+        isActive:true,
+        ulink:"https://www.youtube.com/watch?v=BqCaoGuRDC4&list=PLT9miexWCpPUZ-xD9s70PhhP37blPSuax&index=4",
+        txtName:"mohib",
+        txtEmail:"mohib@gamil.com",
+        htmlContent:"<p>this is html content</p> "
+
+    }
+    
+})
