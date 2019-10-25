299c641bc72f92d28590c7d8d4faeba1d2f86868
unknown <newmohib@gmail.com>
2019-10-25 20:21:15 +0600

    added mouse over out move

--- app6.js
+++ app6.js
@@ -1,12 +1,20 @@
 var myapp= new Vue({
     el:"#my-app6",
     data:{
-       
+       x:0,
+       y:0
     },
     methods:{
-        increment: function(){
-            
-        }
+        movefunction: function(event){
+           this.x=event.offsetX;
+           this.y=event.offsetY;
+        },
+      overfunction: function(event){
+        console.log("mouse over");
+      },
+      outfunction: function(event){
+        console.log("mouse out");
+      }
     }
     
 })
